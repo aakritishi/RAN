d9ae9ee385bf06c1f669752065aebaa103303a8b
sabin gautam <sabin147369@gmail.com>
2024-08-13 14:40:02 +0545

    navbar

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
           <a className="text-gray-700 hover:text-blue-600" href="#">Home</a>
           <div className="relative">
             <button className="text-gray-700 hover:text-blue-600 focus:outline-none" aria-expanded="false">
-              About Us
+              Robotics
               <svg className="inline w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
               </svg>
@@ -46,20 +46,7 @@ export default function Header() {
               <li><a className="block px-4 py-2 text-gray-700 hover:bg-gray-100" href="#">Something else here</a></li>
             </ul>
           </div>
-          <div className="relative">
-            <button className="text-gray-700 hover:text-blue-600 focus:outline-none" aria-expanded="false">
-              Our Services
-              <svg className="inline w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
-              </svg>
-            </button>
-            <ul className="absolute hidden mt-2 space-y-2 bg-white shadow-lg">
-              <li><a className="block px-4 py-2 text-gray-700 hover:bg-gray-100" href="#">Action</a></li>
-              <li><a className="block px-4 py-2 text-gray-700 hover:bg-gray-100" href="#">Another action</a></li>
-              <li><hr className="border-t border-gray-200" /></li>
-              <li><a className="block px-4 py-2 text-gray-700 hover:bg-gray-100" href="#">Something else here</a></li>
-            </ul>
-          </div>
+          <a className="text-gray-700 hover:text-blue-600" href="#">Carrers</a>
           <a className="text-gray-700 hover:text-blue-600" href="#">Contact Us</a>
           <a className="text-gray-700 hover:text-blue-600" href="#">Login</a>
         </div>
