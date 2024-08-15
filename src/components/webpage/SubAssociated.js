d08c5ec389eb0a737be03758f028d46d0675e80d
aakriti <aakritip512@gmail.com>
2024-08-15 14:15:29 +0545

    associated section

--- src/components/webpage/SubAssociated.js
+++ src/components/webpage/SubAssociated.js
@@ -7,7 +7,7 @@ export const SubAssociated = ({ link, image, title }) => {
         <img 
           src={image} 
           alt={title} 
-          className='mx-auto w-full max-w-xs sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] p-0 hover:p-2 transition-all duration-300' 
+          className='mx-auto w-40 h-40 max-w-[250px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] p-0 hover:p-2 transition-all duration-300 rounded-full object-cover' 
         />
       </a>
       <h2 className='text-lg font-semibold mt-2'>{title}</h2>
